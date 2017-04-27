79a00b55465a063034770b7b4a0ec7f23ba07a69
andytang108 <tang147@purdue.edu>
2017-04-27 19:32:32 -0400

    DONE

--- Final/app.py
+++ Final/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import struct
 import time
-import sys, glob  # for listing serial ports
+import threading
 
 try:
     import serial
@@ -15,8 +15,16 @@ connection = None
 VELOCITYCHANGE = 200
 ROTATIONCHANGE = 300
 
+
+
 serial_port = '/dev/tty.usbserial-DA01NW1L'
 
+
+class direction():
+    forward = False
+    backward = False
+    right = False
+    left = False
 
 class tetherDrive():
     callbackKeyUp = False
@@ -26,10 +34,8 @@ class tetherDrive():
     callbackKeyLastDriveCommand = ''
 
     def __init__(self):
-        self.header = 'LET DRIVE'
         self.connection, self.port = self.onConnect()
-        if connection is not None:
-            self.moveForward()
+
 
 
     # sendCommandRaw takes a string interpreted as a byte array
@@ -42,6 +48,7 @@ class tetherDrive():
             else:
                 print("Not connected.")
         except serial.SerialException:
+            self.connection = "Lost connection"
             print("Lost connection")
             connection = None
 
@@ -54,6 +61,7 @@ class tetherDrive():
         try:
             return struct.unpack(fmt, connection.read(n))[0]
         except serial.SerialException:
+            self.connection = "Lost connection"
             print("Lost connection")
             connection = None
             return None
@@ -67,24 +75,89 @@ class tetherDrive():
             cmd += chr(int(v))
 
         self.sendCommandRaw(cmd.encode())
-
-    def moveForward(self):
-        self.sendCommandASCII('131')
-        self.sendCommandASCII('140 3 1 64 16 141 3')
-
+        
+        
+    def robotChange(self, c):
         velocity = 0
-        velocity += VELOCITYCHANGE
-
         rotation = 0
+
+        if c == '0':  # forward
+            pass
+        elif c == '1':  # forward
+            velocity += VELOCITYCHANGE
+        elif c == '2':  # back
+            velocity -= VELOCITYCHANGE
+        elif c == '3':  # left
+            rotation += ROTATIONCHANGE
+        elif c == '4':  # right
+            rotation -= ROTATIONCHANGE
+        elif c == 'p':  # Passive
+            self.sendCommandASCII('128')
+        elif c == 's':  # Safe
+            self.sendCommandASCII('131')
+        elif c == 'f':  # Full
+            self.sendCommandASCII('132')
+        elif c == 'c':  # Clean
+            self.sendCommandASCII('135')
+        elif c == 'd':  # Docmode
+            self.sendCommandASCII('143')
+        elif c == 'b':  # Beep
+            self.sendCommandASCII('140 3 1 64 16 141 3')
+            print(c)
+        elif c == 'r':  # Reset
+            self.sendCommandASCII('7')
+        else:
+            print(repr(c), "not handled")
+
+        vr = velocity + (rotation / 2)
+        vl = velocity - (rotation / 2)
+
+        # create drive command
+        cmd = struct.pack(">Bhh", 145, int(vr), int(vl))
+        self.sendCommandRaw(cmd)
+
+    def robotModeChange(self, mode):
+        if mode == 'P':  # Passive
+            self.sendCommandASCII('128')
+        elif mode == 'S':  # Safe
+            self.sendCommandASCII('131')
+        elif mode == 'F':  # Full
+            self.sendCommandASCII('132')
+        elif mode == 'C':  # Clean
+            self.sendCommandASCII('135')
+        elif mode == 'D':  # Docmode
+            self.sendCommandASCII('143')
+        elif mode == 'SPACE':  # Beep
+            self.sendCommandASCII('140 3 1 64 16 141 3')
+        elif mode == 'R':  # Reset
+            self.sendCommandASCII('7')
+        else:
+            print(repr(mode), "not handled")
+
+    def robotMotionChange(self, d):
+        velocity = 0
+        rotation = 0
+        if d == '1':  # forward
+            velocity += VELOCITYCHANGE
+        elif d == '2':  # back
+            velocity -= VELOCITYCHANGE
+        elif d == '3':  # left
+            rotation += ROTATIONCHANGE
+        elif d == '4':  # right
+            rotation -= ROTATIONCHANGE
+        # +forward -backward
+        # +left -right
 
         # compute left and right wheel velocities
         vr = velocity + (rotation / 2)
         vl = velocity - (rotation / 2)
 
         # create drive command
-        cmd = ("145 0 200 0 200")
+        cmd = struct.pack(">Bhh", 145, int(vr), int(vl))
+        self.sendCommandRaw(cmd)
+        time.sleep(1)
 
-        self.sendCommandASCII(cmd)
+
 
 
 
@@ -98,17 +171,26 @@ class tetherDrive():
             print("Trying " + str(port) + "... ")
             try:
                 connection = serial.Serial(port, baudrate=115200, timeout=1)
+                print("Connected!")
                 return 0, port
             except:
                 return 1, "No connection"
 
 
+@app.route('/<cmd>')
+def getCommand(cmd):
+    runRobot.robotChange(cmd)
+    return "Success"
+
+
 @app.route('/')
 def index():
-    app = tetherDrive()
-    return render_template('index.html', header=app.header, status=app.connection,
-                           port=app.port)
+    return render_template('index.html', header="CNIT425 Final Project", status=runRobot.connection,
+                           port=runRobot.port)
 
 
 if __name__ == '__main__':
+    runRobot = tetherDrive()
+    print("Robot Thread Started...")
     app.run(debug=True, host='0.0.0.0')
+
